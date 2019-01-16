d6fa7461b28eb8118243d738d039917334e747fc
Rhythm Lunatic <kirbyrulez@att.net>
2019-01-15 23:55:44 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import discord
 import json
 import re
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 client = discord.Client()
 DISCORD_TOKEN=""
 COMMAND_NAME="$gfsearch"
+PIC_DOMAIN=""
+
 SITE_DOMAIN = "https://en.gfwiki.com"
 gfcolors = [0, 0, 0xffffff, 0x6bdfce, 0xd6e35a, 0xffb600, 0xdfb6ff]
 def num2stars(n):
@@ -47,7 +51,12 @@ async def on_server_remove(server):
 async def on_message(message):
 	if message.author == client.user:
 		return
-	if message.content.startswith(COMMAND_NAME+" "):
+	if message.content.startswith("$updatestatus"):
+		st = serverCount()
+		print("Attempting to change the status to " + st)
+		await client.change_presence(game=discord.Game(name="testing"))
+		print("done")
+	elif message.content.startswith(COMMAND_NAME+" "):
 		#The string "$gfsearch2 " is 11 characters, so cut it off
 		param = message.content[(len(COMMAND_NAME)+1):].lower()
 		print(param)
@@ -57,25 +66,31 @@ async def on_message(message):
 				embed = discord.Embed(title="No."+str(doll["num"])+" - "+ doll['name'] + " " + num2stars(doll['rating']), url=SITE_DOMAIN+doll['url'], color=gfcolors[doll['rating']])
 				embed.add_field(name="Type", value=doll['type'], inline=True)
 				#{ "hp" : 40, "ammo": 10, "ration": 10, "dmg": 12, "acc": 6, "eva": 9, "rof": 31, "spd": 15, "armor": 0, "crit_rate": 20, "crit_dmg": 50, "pen": 10},
-				embed.add_field(name="Base Stats", value="HP: "+str(doll['baseStats']['hp']) + ", DMG: " + str(doll['baseStats']['dmg']) + ", ACC: " + str(doll['baseStats']['acc']) + ", EVA: " + str(doll['baseStats']['eva']) + ", ROF: " + str(doll['baseStats']['rof']))
-				embed.add_field(name="Max Stats", value="HP: "+str(doll['maxStats']['hp']) + ", DMG: " + str(doll['maxStats']['dmg']) + ", ACC: " + str(doll['maxStats']['acc']) + ", EVA: " + str(doll['maxStats']['eva']) + ", ROF: " + str(doll['maxStats']['rof']))
+				embed.add_field(name="Base Stats", value="**HP:** "+str(doll['baseStats']['hp']) + ", **DMG:** " + str(doll['baseStats']['dmg']) + ", **ACC:** " + str(doll['baseStats']['acc']) + ", **EVA:** " + str(doll['baseStats']['eva']) + ", **ROF:** " + str(doll['baseStats']['rof']))
+				embed.add_field(name="Max Stats", value="**HP:** "+str(doll['maxStats']['hp']) + ", **DMG:** " + str(doll['maxStats']['dmg']) + ", **ACC:** " + str(doll['maxStats']['acc']) + ", **EVA:** " + str(doll['maxStats']['eva']) + ", **ROF:** " + str(doll['maxStats']['rof']))
 				obtain_info = ""
 				if 'stage' in doll['production']:
-					obtain_info += 'STAGE: ' + doll['production']['stage'] + "\n"
+					obtain_info += '**STAGE:** ' + doll['production']['stage'] + "\n"
 				if 'reward' in doll['production']:
-					obtain_info += 'REWARD: ' + doll['production']['reward'] + "\n"
+					obtain_info += '**REWARD:** ' + doll['production']['reward'] + "\n"
 				if 'timer' in doll['production']:
-					obtain_info += 'Production Timer: ' + doll['production']['timer']
+					obtain_info += '**Production Timer:** ' + doll['production']['timer']
+				if doll['name'] in bonusdex and 'drop_rate' in bonusdex[doll['name']]:
+					obtain_info += "\n**Normal Prod. Drop Rate:** " + str(bonusdex[doll['name']]['drop_rate'])+"% (Estimated)"
 				embed.add_field(name="How To Obtain", value=obtain_info,inline=False)
+
 				if 'normal' in doll['production']:
 					a = doll['production']['normal']
-					f = "Manpower: "+str(a['manpower']) + " Ammo: " + str(a['ammo']) + " Rations: " + str(a['rations']) + " Parts: " + str(a['parts'])
-					if doll['production']['placebo'] == True:
-						f += "\n*Production requirements are placebo & may not increase drop rate"
+					f = "**Manpower:** "+str(a[0]) + " **Ammo:** " + str(a[1]) + " **Rations:** " + str(a[2]) + " **Parts:** " + str(a[3])
+					if doll['name'] in bonusdex and 'voodoo' in bonusdex[doll['name']]:
+						a = bonusdex[doll['name']]['voodoo']
+						f += "\nVoodoo: " + "**Manpower:** "+str(a[0]) + " **Ammo:** " + str(a[1]) + " **Rations:** " + str(a[2]) + " **Parts:** " + str(a[3])
+						f += "\n*Voodoo recipes are placebo & may not increase drop rate*"
+				
 					embed.add_field(name="Normal Production Requirement", value=f, inline=True)
 				if 'heavy' in doll['production']:
 					a = doll['production']['heavy']
-					f = "Manpower: "+str(a['manpower']) + " Ammo: " + str(a['ammo']) + " Rations: " + str(a['rations']) + " Parts: " + str(a['parts'])
+					f = "**Manpower:** "+str(a[0]) + " **Ammo:** " + str(a[1]) + " **Rations:** " + str(a[2]) + " **Parts:** " + str(a[3])
 					embed.add_field(name="Heavy Production Requirement", value=f, inline=True)
 				
 				abilityText = doll['ability']['text']
@@ -89,23 +104,41 @@ async def on_message(message):
 					else:
 						break
 				if 'cooldown' in doll['ability']:
-					abilityText += "\nCooldown: " + str(doll['ability']['cooldown'][-1]) + " seconds"
-				embed.add_field(name="Ability: "+doll['ability']['name'], value=abilityText, inline=False)
+					abilityText += "\n**Cooldown:** " + str(doll['ability']['cooldown'][-1]) + " seconds"
+				if 'initial' in doll['ability']:
+					abilityText += ", "+str(doll['ability']['initial']) + " seconds initial cooldown"
+				embed.add_field(name="Skill: "+doll['ability']['name'], value=abilityText, inline=False)
 				
 				embed.add_field(name="Tile Bonus", value=doll['tile_bonus'], inline=True)
 				embed.add_field(name="Tile Bonus Ability", value=doll['bonus_desc'], inline=True)
 				#embed.add_field(name="Tile Bonus Types", value=doll['bonus_type'], inline=True)
-				embed.set_footer(text="Data is Ⓒ en.gfwiki.com and licenced under CC BY-SA 3.0.")
+				if doll['name'] in bonusdex and 'flavor' in bonusdex[doll['name']]:
+					embed.set_footer(text=bonusdex[doll['name']]['flavor'])
+				else:
+					embed.set_footer(text="Data is Ⓒ en.gfwiki.com and licenced under CC BY-SA 3.0.")
+
+				if 'img' in doll:
+					embed.set_image(url=PIC_DOMAIN+doll['img'])
+					print(PIC_DOMAIN+doll['img'])
 				await client.send_message(message.channel, embed=embed)
 				return
-			
-		await client.send_message(message.channel, "No T-Doll was found with that name.")
+		res = process.extract(param, [doll['name'] for doll in frontlinedex], limit=3)
+		if res:
+			print(res)
+			await client.send_message(message.channel, "No T-Doll was found with that name. Did you mean: "+", ".join([i[0] for i in res]))
+		else:
+			print("WARN: No T-Doll was found for " + param)
+			await client.send_message(message.channel, "No T-Doll was found with that name.")
 		return
 		
 #startup...
 print("Reading the frontlinedex into memory. This may take a while.")
 file_obj = open("girlsfrontline.json", "r")
 frontlinedex = json.loads(file_obj.read())
+file_obj.close()
+print("Reading bonus information...")
+file_obj = open("gf_flavortext.json", "r")
+bonusdex = json.loads(file_obj.read())
 file_obj.close()
 print("Done!")
 client.run(DISCORD_TOKEN)
